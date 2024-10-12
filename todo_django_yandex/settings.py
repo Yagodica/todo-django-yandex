4c019e46ec1e3e33154890018c72cfd3ca4ae2f9
yagoda <ivantey228@gmail.com>
2024-10-12 09:20:53 +0300

    css soidet

--- todo_django_yandex/settings.py
+++ todo_django_yandex/settings.py
@@ -91,23 +91,23 @@ WSGI_APPLICATION = 'todo_django_yandex.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": env('DATABASE_DB'),
-#         "USER": env('DATABASE_USER'),
-#         "PASSWORD": env('DATABASE_PASSWORD'),
-#         "HOST": env('DATABASE_HOST'),
-#         "PORT": "5432",
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env('DATABASE_DB'),
+        "USER": env('DATABASE_USER'),
+        "PASSWORD": env('DATABASE_PASSWORD'),
+        "HOST": env('DATABASE_HOST'),
+        "PORT": "5432",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
