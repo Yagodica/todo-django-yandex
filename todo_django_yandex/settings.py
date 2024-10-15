1b505160a78bbe93a02461064e0fda0bd422f365
yagoda <ivantey228@gmail.com>
2024-10-15 19:17:55 +0300

    ya.ru smtp ready

--- todo_django_yandex/settings.py
+++ todo_django_yandex/settings.py
@@ -167,10 +167,15 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.yandex.com'
+EMAIL_HOST = 'smtp.yandex.ru'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
 EMAIL_HOST_USER = 'yagodadaivan@yandex.ru'
 EMAIL_HOST_PASSWORD = 'nzbhffppnmyfnbak'
-DEFAULT_FROM_EMAIL = 'yagodadaivan@yandex.ru'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+SERVER_EMAIL = EMAIL_HOST_USER
+EMAIL_ADMIN = EMAIL_HOST_USER
