e1e061268665e38ee3e515f0fd70011655762cd2
Yagodica <ivantey228@gmail.com>
2024-10-15 14:24:43 +0300

    settings ok

--- todo_django_yandex/settings.py
+++ todo_django_yandex/settings.py
@@ -165,3 +165,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.com'
+EMAIL_PORT = 465
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'yagodadaivan@yandex.ru'
+EMAIL_HOST_PASSWORD = 'nzbhffppnmyfnbak'
